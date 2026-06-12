1bb8433ca034283527561eb0d1bb09d65df4d3e6
Gordon Copestake <gordon@copestake.org>
2026-06-12 19:39:22 +0100

    Switch to build+preview mode with legacy plugin for Tauri webview compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
+import legacy from "@vitejs/plugin-legacy";
 
 const host = process.env.TAURI_DEV_HOST || "0.0.0.0";
 
 export default defineConfig(async () => ({
   clearScreen: false,
+  plugins: [
+    legacy({
+      targets: ["defaults", "not IE 11"],
+    }),
+  ],
   server: {
     port: 1420,
     strictPort: true,
